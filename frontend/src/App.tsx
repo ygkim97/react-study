d360f9ed73b51cd69be9059cffd063e5604aed4f
ygkim97 <ygkim97>
2023-05-10 15:10:36 +0900

    feat: tsx 기초 문법 - tsx 기초 문법 적용해보기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let title = "react + typescript";
+  let style = { color: "red", fontSize: "30px" };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="nav">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>{title}</div>
+      </div>
+      <p style={style}>TEST</p>
     </div>
   );
 }
